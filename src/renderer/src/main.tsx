0d9fa75d5e3843877c877d0ec687dd79f1f00658
Filiph Siitam Sandström <filfat@hotmail.se>
2026-06-02 09:29:00 +0200

    feat(console): mount floating console root on #console route

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,15 +4,18 @@ import './i18n'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import { ConsoleWindow } from './console-window'
 import { TooltipProvider } from './ui/tooltip'
 import { initAccent } from './theme'
 
 initAccent()
 
+// The same bundle backs both the main window and the floating console window;
+// the `#console` hash selects which root to mount.
+const isConsoleWindow = window.location.hash === '#console'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
+    <TooltipProvider>{isConsoleWindow ? <ConsoleWindow /> : <App />}</TooltipProvider>
   </StrictMode>
 )
